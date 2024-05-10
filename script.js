5f1a6846ecc2f81bba88c1dd06b4651d51c2009e
MatijaVukadinovic <matija.vukadinovic@gmail.com>
2024-05-10 16:39:50 +0200

    Declare API key variable

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const weatherForm = document.querySelector("weatherForm");
 const cityInput = document.querySelector("cityInput");
 const card = document.querySelector("card");
+
+const apiKey = "2383705dda8fd853f3c4fa352dac0ed1";
