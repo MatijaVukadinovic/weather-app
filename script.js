07b0a77e4f9a6b6ae52d64ef994e52277f8d531c
MatijaVukadinovic <matija.vukadinovic@gmail.com>
2024-05-10 16:41:10 +0200

    Add event listener to weather form

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const cityInput = document.querySelector("cityInput");
 const card = document.querySelector("card");
 
 const apiKey = "2383705dda8fd853f3c4fa352dac0ed1";
+
+weatherForm.addEventListener("submit", event => {
+    
+})
